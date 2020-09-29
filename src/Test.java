b2e7e794e1ebe31e2d9560bce81e53c3bed87efc
user <user@EB07>
2020-09-29 09:52:47 +0800

    自我介紹

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+  /**
+   * args.
+   * This is the introdution of mine.
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("I'm ªL·Ø¼ý");
+    System.out.println("My Nid is D0845073");
+    System.out.println("This is lab 1 hw");
+  }
+
+}
